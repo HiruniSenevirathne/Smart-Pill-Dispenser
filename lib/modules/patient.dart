3169742261857dc90731fa145d3b88490817e69e
HiruniSenevirathne <senevirathnehu@gmail.com>
2022-04-17 19:23:01 +0530

    commit

--- lib/modules/patient.dart
+++ lib/modules/patient.dart
@@ -1,21 +1,18 @@
 class Patients {
   late String patientId;
   late String patientName;
-  // late String patientPhoneNo;
   late String patientAge;
   String? image;
 
   Patients(
       {required this.patientId,
       required this.patientName,
-      // required this.patientPhoneNo,
       required this.patientAge,
       this.image});
 
   Patients.fromJson(Map<String, dynamic> json) {
     patientId = json['patient_id'];
     patientName = json['patient_name'];
-    // patientPhoneNo = json['patient_phoneNo'];
     patientAge = json['patient_age'];
     image = json['image'];
   }
@@ -24,7 +21,6 @@ class Patients {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.patientId;
     data['patient_name'] = this.patientName;
-    // data['patient_phoneNo'] = this.patientPhoneNo;
     data['patient_age'] = this.patientAge;
     data['image'] = this.image;
     return data;
